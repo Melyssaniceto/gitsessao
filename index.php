168e1ee9722f1417213b31da3c1b404642c025b9
Melyssaniceto <anicetomelyssa@gmail.com>
2023-10-20 13:30:09 -0300

    dashboard

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
+    <title>Sessão</title>
 </head>
 <body>
-    <h1>index</h1>
+    <h1>TESTE - 01</h1>
+
 </body>
 </html>
